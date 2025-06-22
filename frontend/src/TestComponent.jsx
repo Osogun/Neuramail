35612f7cc14fcadf83598bd6f52eb06480f80197
Oskar Gumowski <81420358+Osogun@users.noreply.github.com>
2025-06-23 01:12:52 +0200

    feat(frontend): add test component

---
+++ frontend/src/TestComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TestComponent = () => (
+  <div className="p-4 bg-green-200 rounded-lg shadow">
+    <p className="text-lg font-semibold">Test React component works!</p>
+  </div>
+);
+
+export default TestComponent;
